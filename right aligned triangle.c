36898c5bf942ff6bab42a543ef387948c4375d39
Raj <hasanraj31000@gmail.com>
2022-06-03 18:29:26 +0600

    Adding left, right and full pyramid code

---
+++ right aligned triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n, i, j, sp;
+    printf("Enter the size of the triangle: \n");
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++) {
+        for(sp=0;sp<n-1-i;sp++) printf("  ");
+        for(j=0;j<=i;j++) {
+            printf(" #");
+        } printf("\n");
+    }
+
+    return 0;
+}
